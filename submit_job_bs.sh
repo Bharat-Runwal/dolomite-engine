d322515cebacc0cbb73df833efbc2ab9d8ff1d28
Bishwajit.Saha3@ibm.com;9J9701897;Bishwajit Saha <bsaha3@login4.bluevela.rmf.ibm.com>
2026-02-13 17:55:57 +0000

    add unshard config and previous stashed changes

--- submit_job_bs.sh
+++ submit_job_bs.sh
@@ -3,7 +3,9 @@
 # Energy-GPT Training Job Submission
 # Usage: bash submit_train.sh [config_file]
 
-CONFIG=${1:-"configs/energy/energy_bs.yml"}
+# CONFIG=${1:-"configs/energy/energy_bs.yml"}
+CONFIG=${1:-"configs/energy/energy_loop_rec_egpt_nemo_bs.yml"}
+
 LOG_DIR="/proj/dmfexp/energy-gpt/logs"
 
 mkdir -p "$LOG_DIR"
@@ -14,10 +16,10 @@ bsub \
   -M 2000G \
   -hl \
   -n 4 \
-  -J bs-energy-v1 \
+  -J bs-energy-nemo \
   -gpu "num=8/task:mode=exclusive_process" \
-  -oo "${LOG_DIR}/bs-energy-train-%J.out" \
-  -eo "${LOG_DIR}/bs-energy-train-%J.err" \
+  -oo "${LOG_DIR}/bs-energy-nemo-%J.out" \
+  -eo "${LOG_DIR}/bs-energy-nemo-%J.err" \
   blaunch bash launch-scripts/pretrain.sh "$CONFIG"
 
 
